d8b704218c8f696383d7d9ce16bb3077681670c4
steven <szapata427@yahoo.com>
2020-05-30 15:30:29 -0400

    successfully added google maps with spotify icon on countries

--- src/components/ComposableMap.js
+++ src/components/ComposableMap.js
@@ -3,6 +3,8 @@ import ReactDOM from "react-dom";
 // import { ComposableMap, Geographies, Geography } from "react-simple-maps";
 // import "./styles.css";
 import {GoogleMap, useLoadScript,Marker, InfoWindow} from "@react-google-maps/api"
+import MapStyles from './MapStyles'
+import SpotifyImageIcon from '../Images/spotifyIcon.png'
 
 const libraries = ["places"]
 const mapContainerStyle = {
@@ -15,6 +17,14 @@ const centerMedellin = {
     lng: -75.581215
 
 }
+
+const options = {
+    styles: MapStyles,
+    disableDefaultUI:true,
+    zoomControl: true
+}
+
+const markers = [{id: 1, lat:4.570868, lng:-74.297333, country: "Colombia"},{id: 2, lat: -38.416097, lng:-63.616672, country: "Argentina"}]
 
 export default function GoogleMaps() {
 
@@ -31,7 +41,19 @@ return (<div>
     <GoogleMap mapContainerStyle={mapContainerStyle}
     zoom={8}
     center={centerMedellin}
-    ></GoogleMap>
+    options={options}
+    >
+     {markers.map(marker => {
+         console.log(marker)
+         return <Marker key={marker.id}
+         position={{lat: marker.lat, lng: marker.lng}}
+         icon={{url: SpotifyImageIcon, scaledSize: new window.google.maps.Size(30,30)}}
+         />
+     })}  
+
+
+
+    </GoogleMap>
 </div>)
 }
 
